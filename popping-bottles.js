894f35470a83766d370815bc002d2b913a4c19c9
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-20 00:34:05 +0000

    proper algorithm to compute the amount of total bottles

--- popping-bottles.js
+++ popping-bottles.js
@@ -1,7 +1,26 @@
 arg = Number(process.argv.slice(2));
 
 function bottleReturn(num) {
+  bottles = num / 2;
+  emptyBottles = bottles;
+  bottleCaps = bottles;
+  totalBottles = bottles;
 
+  while(bottleCaps > 3 || emptyBottles > 1) {
+
+    if(emptyBottles >= 2) {
+      emptyBottles -= 1;
+      bottleCaps += 1;
+      totalBottles += 1;
+    }
+
+    if(bottleCaps >= 4) {
+      bottleCaps -= 3;
+      emptyBottles += 1;
+      totalBottles += 1;
+    }
+  }
+  console.log(totalBottles);
 }
 
 bottleReturn(arg);
